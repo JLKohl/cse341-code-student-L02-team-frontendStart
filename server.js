55f002d818126f57d0752ed52c1b0664fafc5f3c
JLKohl <121449936+JLKohl@users.noreply.github.com>
2026-01-27 19:18:37 -0600

    trying to fix front end on render

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors()); 
 app.use(express.json());
-app.use(express.static(__dirname + '/frontend'));
+app.use(express.static(__dirname + './frontend'));
 
 
 app.use('/professional', professionalRoutes);
